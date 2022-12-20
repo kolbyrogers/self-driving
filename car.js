d7f3a4dd96a0257c22685f46e04f8d4a999d341b
Kolby Rogers <kolbyrogers1@gmail.com>
2022-12-20 14:33:10 -0700

    implement angles

--- car.js
+++ car.js
@@ -8,6 +8,7 @@ class Car {
     this.acceleration = 0.2;
     this.maxSpeed = 5;
     this.friction = 0.05;
+    this.angle = 0;
 
     this.Controls = new Controls();
   }
@@ -15,6 +16,8 @@ class Car {
   update() {
     this.Controls.forward ? (this.speed += this.acceleration) : null;
     this.Controls.reverse ? (this.speed -= this.acceleration) : null;
+    this.Controls.left ? (this.angle += 0.03) : null;
+    this.Controls.right ? (this.angle -= 0.03) : null;
 
     this.speed > this.maxSpeed ? (this.speed = this.maxSpeed) : null;
     this.speed < -this.maxSpeed / 2 ? (this.speed = -this.maxSpeed / 2) : null;
@@ -24,12 +27,19 @@ class Car {
 
     Math.abs(this.speed) < this.friction ? (this.speed = 0) : null;
 
-    this.y -= this.speed;
+    this.x -= Math.sin(this.angle) * this.speed;
+    this.y -= Math.cos(this.angle) * this.speed;
   }
 
   draw(ctx) {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(-this.angle);
+
     ctx.beginPath();
-    ctx.rect(this.x - this.width / 2, this.y - this.height / 2, this.width, this.height);
+    ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
     ctx.fill();
+
+    ctx.restore();
   }
 }
