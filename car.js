12147bde68f4cff85b73bed995e1d233c5c887d5
Kolby Rogers <kolbyrogers1@gmail.com>
2022-12-20 18:43:31 -0700

    add sensors

--- car.js
+++ car.js
@@ -10,11 +10,13 @@ class Car {
     this.friction = 0.05;
     this.angle = 0;
 
+    this.sensor = new Sensor(this);
     this.Controls = new Controls();
   }
 
   update() {
     this.#move();
+    this.sensor.update();
   }
 
   #move() {
@@ -48,5 +50,7 @@ class Car {
     ctx.fill();
 
     ctx.restore();
+
+    this.sensor.draw(ctx);
   }
 }
---
+++ sensor.js
@@ -0,0 +1,38 @@
+class Sensor {
+  constructor(car) {
+    this.car = car;
+    this.rayCount = 3;
+    this.rayLength = 100;
+    this.raySpread = Math.PI / 4;
+
+    this.rays = [];
+  }
+
+  update() {
+    this.rays = [];
+    for (let i = 0; i < this.rayCount; i++) {
+      const angle = lerp(
+        this.raySpread / 2,
+        -this.raySpread / 2,
+        i / (this.rayCount - 1)
+      );
+      const start = { x: this.car.x, y: this.car.y };
+      const end = {
+        x: this.car.x - Math.sin(angle) * this.rayLength,
+        y: this.car.y - Math.cos(angle) * this.rayLength,
+      };
+      this.rays.push([start, end]);
+    }
+  }
+
+  draw(ctx) {
+    for (let i = 0; i < this.rayCount; i++) {
+      ctx.beginPath();
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = "yellow";
+      ctx.moveTo(this.rays[i][0].x, this.rays[i][0].y);
+      ctx.lineTo(this.rays[i][1].x, this.rays[i][1].y);
+      ctx.stroke();
+    }
+  }
+}
