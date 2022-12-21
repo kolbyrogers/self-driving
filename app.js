95b2805f956938ede4bca471e8eadf821394e394
Kolby Rogers <kolbyrogers1@gmail.com>
2022-12-20 18:35:08 -0700

    implement 'camera pov' logic

--- app.js
+++ app.js
@@ -11,7 +11,13 @@ function animate() {
   car.update();
 
   canvas.height = window.innerHeight;
+
+  ctx.save();
+  ctx.translate(0, -car.y + canvas.height * 0.7);
+
   road.draw(ctx);
   car.draw(ctx);
+
+  ctx.restore();
   requestAnimationFrame(animate);
 }
