0020ccf92030801809e09250ebd3defbfab82c29
Kolby Rogers <kolbyrogers1@gmail.com>
2022-12-22 19:08:48 -0700

    add save and remove brain functionality

--- app.js
+++ app.js
@@ -7,16 +7,32 @@ const carCtx = carCanvas.getContext("2d");
 const networkCtx = networkCanvas.getContext("2d");
 
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
+const traffic = [
+  new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(2), -300, 30, 50, "DUMMY", 2),
+];
 
 const cars = generateCars(100);
-const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)];
+let bestCar = cars[0];
+if (localStorage.getItem("bestBrain")) {
+  bestCar.brain = JSON.parse(localStorage.getItem("bestBrain"));
+}
 
 animate();
+
+function saveBrain() {
+  localStorage.setItem("bestBrain", JSON.stringify(bestCar.brain));
+}
+
+function discardBrain() {
+  localStorage.removeItem("bestBrain");
+}
 
 function generateCars(n) {
   const cars = [];
   for (let i = 1; i < n; i++) {
-    cars.push(new Car(road.getLaneCenter(1), 100, 30, 50, "AI", 2));
+    cars.push(new Car(road.getLaneCenter(1), 100, 30, 50, "AI", 4));
   }
   return cars;
 }
@@ -28,7 +44,7 @@ function animate(time) {
   for (let i = 0; i < cars.length; i++) {
     cars[i].update(road.borders, traffic);
   }
-  const bestCar = cars.find((e) => e.y == Math.min(...cars.map((e) => e.y)));
+  bestCar = cars.find((e) => e.y == Math.min(...cars.map((e) => e.y)));
 
   carCanvas.height = window.innerHeight;
   networkCanvas.height = window.innerHeight;
